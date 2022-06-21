66ffc961def2b611595a157b1581a4fd7aac34cd
Ara'a Smadi <100359539+AraaSmadi@users.noreply.github.com>
2022-06-21 21:12:58 +0300

    Update Cards.js

--- 21-JUNE-2022Tasks/Task2/src/Components/Cards.js
+++ 21-JUNE-2022Tasks/Task2/src/Components/Cards.js
@@ -16,7 +16,7 @@ class Cards extends React.Component {
 
   render() {
     return (
-          <div class="col-sm-4">
+          <div class="col-sm-3">
         <img src="logo192.png"/>
         <h3>{this.props.name} </h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
