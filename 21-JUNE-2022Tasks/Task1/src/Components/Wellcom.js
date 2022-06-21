7bd7cc2026f498a3e3ae9a6824d620d2dc6d86fc
Ara'a Smadi <100359539+AraaSmadi@users.noreply.github.com>
2022-06-21 16:02:21 +0300

    Update Wellcom.js

--- 21-JUNE-2022Tasks/Task1/src/Components/Wellcom.js
+++ 21-JUNE-2022Tasks/Task1/src/Components/Wellcom.js
@@ -1,7 +1,7 @@
 function Wellcom() {
     const st = { color: "red" }
     return ( <div className="container">< h3 > Title: Wellcom </h3>
-    <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry 's standard dummy.</p>
+    <p style={st}> Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry 's standard dummy.</p>
     </div>
      );
     }
